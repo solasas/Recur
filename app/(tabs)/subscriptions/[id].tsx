9734754cc155068839f25008610659dc057c68b5
sashank solasa <sashanksolasa@gmail.com>
2026-04-03 18:25:42 +0530

    created the outline for all basic tabs required for the project

---
+++ app/(tabs)/subscriptions/[id].tsx
@@ -0,0 +1,15 @@
+import {View,Text} from 'react-native';
+import React from 'react';
+import {useLocale} from "@react-navigation/native";
+import {Link, useLocalSearchParams} from "expo-router";
+
+const SubscriptionDetails =()=>{
+    const {id}=useLocalSearchParams<{id: string}>();
+    return(
+        <View>
+            <Text>SubscriptionDetails : {}</Text>
+            <Link href={"/"}>Go Back</Link>
+        </View>
+    )
+}
+export default SubscriptionDetails;
